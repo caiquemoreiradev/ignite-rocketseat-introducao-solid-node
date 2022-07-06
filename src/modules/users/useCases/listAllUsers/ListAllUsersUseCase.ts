78ae5adf73c86156d04daf3b189159e022e887f8
Caique Moreira <caiquemoreiradev@gmail.com>
2022-07-06 15:08:52 -0300

    [Caique] - challenge done

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -6,10 +6,27 @@ interface IRequest {
 }
 
 class ListAllUsersUseCase {
-  constructor(private usersRepository: IUsersRepository) {}
+  constructor(private usersRepository: IUsersRepository) { }
 
   execute({ user_id }: IRequest): User[] {
+
     // Complete aqui
+
+    const user = this.usersRepository.findById(user_id);
+
+    if (!user) {
+
+      throw new Error("Usuário não encontrado!");
+    }
+
+    if (user.admin === false) {
+
+      throw new Error("Apenas admins podem listar todos os usuários");
+    }
+
+    const users = this.usersRepository.list();
+
+    return users;
   }
 }
 
